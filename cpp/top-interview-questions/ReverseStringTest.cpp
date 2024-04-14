53c7e4e9a93f4442671ee599fc692b09fdb7c8b6
daniel-lind <daniel.lind@king.com>
2024-04-14 10:49:26 +0200

    top interview questions - reverse string cpp

--- cpp/top-interview-questions/ReverseStringTest.cpp
+++ cpp/top-interview-questions/ReverseStringTest.cpp
@@ -1,8 +1,22 @@
 #include <gtest/gtest.h>
+#include <vector>
 
-#include <string>
+using namespace std;
+
+namespace {
+void reverseString(vector<char>& s) {
+  std::size_t n = s.size();
+  auto mid = n / 2;
+  for (auto i=0; i < mid; ++i) {
+    std::swap(s[i], s[n-i-1]);
+  }
+}
+}
 
 TEST(ReverseString, HelloTest)
 {
-    ASSERT_FALSE(true);
+  vector<char> actual = {'h', 'e', 'l', 'l', 'o'};
+  reverseString(actual);
+  vector<char> expected = {'o', 'l', 'l', 'e', 'h'};
+  ASSERT_EQ(expected, actual);
 }
