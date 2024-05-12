ad73eabc05cde739f02893924c404822d54b127b
daniel-lind <daniel.lind@king.com>
2024-05-12 18:17:00 +0200

    Improved MaxSlidingWindow that uses deque

--- interview-questions-cpp/other/MaxSlidingWindow.cpp
+++ interview-questions-cpp/other/MaxSlidingWindow.cpp
@@ -50,7 +50,7 @@ auto max_heap_compare = [](const Node &left, const Node &right) {
 
 class Solution {
 public:
-  vector<int> maxSlidingWindow(vector<int> &nums, int k) {
+  vector<int> maxSlidingWindowPrioQueue(vector<int> &nums, int k) {
     vector<int> result;
 
     using container = std::vector<Node>;
@@ -71,7 +71,39 @@ public:
 
     return result;
   }
+
+  vector<int> maxSlidingWindow(vector<int> &nums, int k) {
+    vector<int> result;
+
+    // Sliding window with indices of nums within the sliding window.
+    deque<int> slidingWindow;
+
+    for (auto i = 0; i < nums.size(); ++i) {
+      // 1. Remove element at the top if it is outside of the window
+      if (!slidingWindow.empty() && slidingWindow.front() <= i - k) {
+        slidingWindow.pop_front();
+      }
+
+      // 2. Maintain the deque so that all items in the queue that is smaller than the current item is removed, they cannot contribute
+      while (!slidingWindow.empty() && nums[slidingWindow.back()] < nums[i]) {
+        slidingWindow.pop_back();
+      }
+
+      // 3. Add the current item to the back of the queue since all smaller items has been removed in step 2
+      slidingWindow.push_back(i);
+
+      // 4. If current item is not within the first k-1 entries add the top element if the deque to the result array
+      if (i >= k - 1) {
+        auto maxElement = slidingWindow.front();
+        result.push_back(nums[maxElement]);
+      }
+    }
+
+    return result;
+  }
 };
+
+
 
 } // namespace
 
@@ -79,16 +111,38 @@ TEST(MaxSlidingWindowTest, TC1) {
   std::vector<int> input = {1,3,-1,-3,5,3,6,7};
   constexpr auto k = 3;
   Solution solution;
-  auto actual = solution.maxSlidingWindow(input, k);
   const std::vector<int> expected  = {3,3,5,5,6,7};
+
+  auto actual = solution.maxSlidingWindowPrioQueue(input, k);
   EXPECT_EQ(expected, actual);
+
+  actual = solution.maxSlidingWindow(input, k);
+  EXPECT_EQ(expected, actual);
+
 }
 
 TEST(MaxSlidingWindowTest, TC2) {
   std::vector<int> input = {1,-1};
   constexpr auto k = 1;
   Solution solution;
-  auto actual = solution.maxSlidingWindow(input, k);
   const std::vector<int> expected  = {1, -1};
+
+  auto actual = solution.maxSlidingWindowPrioQueue(input, k);
+  EXPECT_EQ(expected, actual);
+
+  actual = solution.maxSlidingWindow(input, k);
+  EXPECT_EQ(expected, actual);
+}
+
+TEST(MaxSlidingWindowTest, TC3) {
+  std::vector<int> input = {7,2,4};
+  constexpr auto k = 2;
+  Solution solution;
+  const std::vector<int> expected  = {7, 4};
+
+  auto actual = solution.maxSlidingWindowPrioQueue(input, k);
+  EXPECT_EQ(expected, actual);
+
+  actual = solution.maxSlidingWindow(input, k);
   EXPECT_EQ(expected, actual);
 }
